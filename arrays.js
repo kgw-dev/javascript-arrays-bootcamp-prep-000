5c024575c98248b87bdf19e77d6265e3ff55ede6
kent winter <kgw700@gmail.com>
2017-11-16 18:27:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,7 @@
-var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
+
 function addElementToBeginningOfArray (x,y) {
-  ["y",...x];
+  addElementToBeginningOfArray.push("y");
   return(x)
 }
 function destructivelyAddElementToBeginningOfArray (x,y) {
